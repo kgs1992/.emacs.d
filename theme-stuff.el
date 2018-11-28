8d0ffa7e69249b670d59d881cf283d15fe58925d
Kiran Shenoy <kiran.shenoy@nutanix.com>
2018-11-28 18:22:22 +0530

    Fixed colours for a few things

--- theme-stuff.el
+++ theme-stuff.el
@@ -19,7 +19,7 @@
 ;; may have their own settings.
 (load-theme 'doom-one t)
 
-;; set monoaki style tabbar
+;; Set tabbar style
 (set-face-attribute
  'tabbar-default nil
  :background "black"
@@ -49,61 +49,7 @@
  :background "black"
  :height 0.1)
 
-;; ;; Change padding of the tabs
-;; ;; we also need to set separator to avoid overlapping tabs by highlighted tabs
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(ansi-color-names-vector
-;;    ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
-;;  '(compilation-message-face (quote default))
-;;  '(custom-safe-themes
-;;    (quote
-;;     ("e9df267a1c808451735f2958730a30892d9a2ad6879fb2ae0b939a29ebf31b63" default)))
-;;  '(fci-rule-color "#3C3D37")
-;;  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
-;;  '(highlight-tail-colors
-;;    (quote
-;;     (("#3C3D37" . 0)
-;;      ("#679A01" . 20)
-;;      ("#4BBEAE" . 30)
-;;      ("#1DB4D0" . 50)
-;;      ("#9A8F21" . 60)
-;;      ("#A75B00" . 70)
-;;      ("#F309DF" . 85)
-;;      ("#3C3D37" . 100))))
-;;  '(magit-diff-use-overlays nil)
-;;  '(pos-tip-background-color "#A6E22E")
-;;  '(pos-tip-foreground-color "#272822")
-;;  '(tabbar-separator (quote (1.0)))
-;;  '(vc-annotate-background nil)
-;;  '(vc-annotate-color-map
-;;    (quote
-;;     ((20 . "#F92672")
-;;      (40 . "#CF4F1F")
-;;      (60 . "#C26C0F")
-;;      (80 . "#E6DB74")
-;;      (100 . "#AB8C00")
-;;      (120 . "#A18F00")
-;;      (140 . "#989200")
-;;      (160 . "#8E9500")
-;;      (180 . "#A6E22E")
-;;      (200 . "#729A1E")
-;;      (220 . "#609C3C")
-;;      (240 . "#4E9D5B")
-;;      (260 . "#3C9F79")
-;;      (280 . "#A1EFE4")
-;;      (300 . "#299BA6")
-;;      (320 . "#2896B5")
-;;      (340 . "#2790C3")
-;;      (360 . "#66D9EF"))))
-;;  '(vc-annotate-very-old-color nil)
-;;  '(weechat-color-list
-;;    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
-
-;; adding spaces
+;; Adding spaces
 (defun tabbar-buffer-tab-label (tab)
   "Return a label for TAB.
 That is, a string used to represent it on the tab bar."
@@ -124,18 +70,25 @@ That is, a string used to represent it on the tab bar."
 (global-font-lock-mode 1)
 (setq font-lock-maximum-decoration t)
 
-;; Helm monokai
+;; Helm colours
 (set-face-attribute 'helm-selection nil
                     :background "grey10"
                     :foreground "blue")
 (set-face-attribute 'helm-source-header nil
                     :background "gray30"
                     :foreground "green")
+
+;; Misc colours
 (set-face-attribute 'region nil
-                    :background "grey10")
+                    :background "blue"
+                    :foreground "black")
 (set-face-background 'hl-line "grey10")
 (set-face-attribute 'magit-diff-hunk-heading nil
                     :foreground "white")
 (set-face-attribute 'magit-diff-hunk-heading-highlight nil
                     :foreground "white"
                     :background "grey10")
+(set-face-attribute 'magit-diff-removed nil
+                    :background "black")
+(set-face-attribute 'magit-diff-removed-highlight nil
+                    :background "black")
