a02bc0eb281c4e7d0451feb447dff3349c429b23
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-06-17 16:17:05 +0530

    Added helm-company

--- init-editing.el
+++ init-editing.el
@@ -68,6 +68,11 @@
   (setq company-begin-commands '(self-insert-command)) ; Start autocompletion only after typing
   :hook (after-init . global-company-mode))
 
+(use-package helm-company
+  :ensure t
+  :defer t
+  :after (helm))
+
 ;; Autocomplete - ac
 ;; (use-package auto-complete
 ;;   :ensure t
