b0b6b69c679dced2e8941ac9a0236e12e8b553a5
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-06-24 13:34:32 +0530

    Added smex

--- init-ivy.el
+++ init-ivy.el
@@ -16,14 +16,22 @@
   :defer t
   :hook (after-init . session-initialize))
 
+;; Smex
+(use-package smex
+  :ensure t
+  :defer t
+  :hook (after-init . smex-initialize))
+
 ;; Ivy
 (use-package counsel
   :ensure t
   :defer t
+  :after (smex)
   :config
+  (setq ivy-ignore-buffers '("\\` " "\\`\\*"))
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-switch-buffer)
-         ("C-x C-f" . counsel-find-files))
+         ("C-x C-f" . counsel-find-file))
   :hook ((after-init . ivy-mode)
          (ivy-mode . counsel-mode)))
 
