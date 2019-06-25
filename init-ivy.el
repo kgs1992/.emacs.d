ab7ab6127d27b477ab9847b916d71de2d65d4f9c
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-06-25 14:13:03 +0530

    Added ivy-rich

--- init-ivy.el
+++ init-ivy.el
@@ -23,17 +23,48 @@
   :hook (after-init . smex-initialize))
 
 ;; Ivy
+(use-package ivy
+  :ensure t
+  :defer t
+  :config
+  (setq ivy-ignore-buffers '("\\` " "\\`\\*"))
+  :bind (("C-x b" . ivy-switch-buffer))
+  :hook (after-init . ivy-mode))
+
 (use-package counsel
   :ensure t
   :defer t
-  :after (smex)
-  :config
-  (setq ivy-ignore-buffers '("\\` " "\\`\\*"))
   :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-switch-buffer)
          ("C-x C-f" . counsel-find-file))
-  :hook ((after-init . ivy-mode)
-         (ivy-mode . counsel-mode)))
+  :hook (after-init . counsel-mode))
+
+(use-package ivy-rich
+  :ensure t
+  :defer t
+  :config
+  (setq ivy-virtual-abbreviate 'full
+        ivy-rich-path-style 'abbrev)
+  (defun ivy-rich-switch-buffer-icon (candidate)
+    (with-current-buffer
+        (get-buffer candidate)
+      (let ((icon (all-the-icons-icon-for-mode major-mode)))
+        (if (symbolp icon)
+            (all-the-icons-icon-for-mode 'fundamental-mode)
+          icon))))
+  (setq ivy-rich-display-transformers-list
+        '(ivy-switch-buffer
+          (:columns
+           ((ivy-rich-switch-buffer-icon :width 2)
+            (ivy-rich-candidate (:width 30))
+            (ivy-rich-switch-buffer-size (:width 7))
+            (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+            (ivy-rich-switch-buffer-project (:width 15 :face success))
+            (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+           :predicate
+           (lambda (cand) (get-buffer cand)))))
+
+  :hook (after-init . ivy-rich-mode))
 
 (message "Loaded init-ivy.el")
 (provide 'init-ivy)
