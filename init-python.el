f08494efee3838664feb851ba1ab0ca5921668c0
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-12-19 14:02:15 +0530

    Pick python flycheck binaries from virtualenv

--- init-python.el
+++ init-python.el
@@ -15,10 +15,17 @@
   :ensure t
   :defer t
   :config
+  (defun set-flychecker-executables ()
+    "Configure virtualenv for flake8 and lint."
+    (flycheck-set-checker-executable (quote python-flake8)
+                                     (locate-file "flake8" exec-path))
+    (flycheck-set-checker-executable (quote python-pylint)
+                                     (locate-file "flake8" exec-path)))
   (defun activate-if-python-mode ()
-      (if (eq (with-current-buffer (current-buffer)
+      (when (eq (with-current-buffer (current-buffer)
                 major-mode) 'python-mode)
-          (auto-virtualenvwrapper-activate)))
+          (auto-virtualenvwrapper-activate)
+          (set-flychecker-executables)))
   :hook ((python-mode . activate-if-python-mode)
          (window-configuration-change . activate-if-python-mode)
          (after-focus . activate-if-python-mode)))
