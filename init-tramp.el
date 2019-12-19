88477d7f7aaff3a9af4d233e710a865dc7d4afcb
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-12-19 14:00:14 +0530

    Improvements for tramp

--- init-tramp.el
+++ init-tramp.el
@@ -12,14 +12,30 @@
 ;;; Code:
 ;; Tramp for remote editing
 (use-package tramp
-  :defer t
+  :demand t
   :config
   (setq tramp-default-method "sshx")
   (setq backup-directory-alist
         `((".*" . ,temporary-file-directory)))
   (setq auto-save-file-name-transforms
         `((".*" ,temporary-file-directory t)))
-  (setq tramp-backup-directory-alist backup-directory-alist))
+  (setq tramp-backup-directory-alist backup-directory-alist)
+  (setq tramp-save-ad-hoc-proxies t)
+  (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+  ;; (setq tramp-chunksize 500)
+  )
+
+
+
+(use-package counsel-tramp
+  :ensure t
+  :demand t
+  :config
+  (add-hook 'counsel-tramp-pre-command-hook '(lambda ()
+                                               (projectile-mode 0)))
+  (add-hook 'counsel-tramp-quit-hook '(lambda ()
+                                        (projectile-mode 1) )))
+
 
 (message "Loaded init-tramp.el")
 (provide 'init-tramp)
